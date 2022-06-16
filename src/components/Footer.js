2d03e4c7238bdeeae736769d0411ce450d32e82a
Sean Watson <seanwatson@MacBook-Pro.local>
2022-06-16 09:14:29 -0600

    components update add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h4>&copy; Buck-T 2022</h4>
+    </footer>
+  )
+}
---
+++ src/components/Mainbody.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MainBody = () => {
+  return(
+    <h1>This is the Main Body</h1>
+  )
+};
+
+export default MainBody;
