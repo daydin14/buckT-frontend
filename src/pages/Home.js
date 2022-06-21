1350a121710a1c458c994e2d0701e693a76cc7da
David Aydin <david.aydin6@gmail.com>
2022-06-21 10:56:55 -0400

    merge conflict

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,10 @@
 const Home = (props) => {
     return(
         <div className="Home">
-<<<<<<< HEAD
             <h1 className="neonText">Welcome to {`<li>Buck-T</li>`}</h1>
             <p className="neonText">{`<li>Buck-T</li>`} is your onse-stop travel bucket list maker.</p>
             <p className="neonText"> Add the destination of your dreams to your list, and begin daydreaming!</p>
             <p className="neonText">Please login to enjoy all the features of our site!</p>
-=======
-            <h1>Welcome to {`<li>Buck-T</li>`}</h1>
-            <p>{`<li>Buck-T</li>`} is your one-stop travel bucket list maker. Add the destination of your dreams to your list, and begin daydreaming!</p>
-            <p>Please login to enjoy all the features of our site!</p>
->>>>>>> refs/remotes/origin/master
         </div>
     );
 };
