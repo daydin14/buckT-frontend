76d2ecc5940c8ee1aa78ffe5cd00c8a27c5d4faa
mahamedaden <joali218@gmail.com>
2022-06-20 21:54:49 -0500

    changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,92 @@
 const About = (props) => {
-    return(
-        <h2> about us page</h2>
-    );
+  return (
+
+    <div className="Picture">
+
+
+      <div className="me"> 
+      <h2 >Mahamed Aden</h2>
+      
+      <img
+        src="https://ca.slack-edge.com/T0351JZQ0-U03CJLBTY9J-c2311a37924c-512"
+        className="Mahamed"
+        alt="..."
+      />
+       
+        <div className="links">
+          <a href="https://github.com/Slithur" target="_blank" rel="noopener noreferrer">
+            <img src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png" className="git" alt="..." />
+          </a>
+          <a href="https://www.linkedin.com/in/mahamed-aden-bba919239/" target="_blank" rel="noopener noreferrer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png" className="linked" alt="..." type="button" />
+          </a>
+        </div>
+      </div>
+
+      <div className="s">
+      <h2>Sean Watson</h2>
+        <img
+        src="https://ca.slack-edge.com/T0351JZQ0-U03C2RA1M09-a259f9c07bbc-512"
+        className="Sean"
+        alt="..."
+      />
+        
+        <div className="links">
+          <a href="https://github.com/sean-watson30" target="_blank" rel="noopener noreferrer">
+            <img src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png" className="git" alt="..." />
+          </a>
+
+          <a href="https://www.linkedin.com/in/sean-watson-080091220/" target="_blank" rel="noopener noreferrer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png" className="linked" alt="..." />
+          </a>
+        </div>
+
+      </div>
+
+
+
+      <div className="d">
+      <h2>David Aydin</h2>
+        <img
+        src="https://ca.slack-edge.com/T0351JZQ0-U03BUJ92GP9-6a68c2797ac1-512"
+        className="David"
+        alt="..."
+      />
+       
+        <div className="links">
+          <a href="https://github.com/daydin14" target="_blank" rel="noopener noreferrer">
+            <img src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png" className="git" alt="..." />
+          </a>
+
+          <a href="https://www.linkedin.com/in/dmaydin/" target="_blank" rel="noopener noreferrer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png" className="linked" alt="..." />
+          </a>
+        </div>
+      </div>
+
+
+      <div className="k">
+      <h2>Min Yong Kim</h2>
+        <img
+        src="https://ca.slack-edge.com/T0351JZQ0-U02U94J0YD6-7acaec2d1067-512"
+        className="Kim"
+        alt="..."
+      />
+        
+        <div className="links">
+          <a href="https://github.com/jykyle0226" target="_blank" rel="noopener noreferrer">
+            <img src="https://cdn-icons-png.flaticon.com/512/5968/5968866.png" className="git" alt="..." />
+          </a>
+          <a href="https://www.linkedin.com/in/min-yong-kim-0226//" target="_blank" rel="noopener noreferrer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png" className="linked" alt="..." />
+          </a>
+        </div>
+
+      </div>
+
+    </div>
+
+  );
 
 };
 
